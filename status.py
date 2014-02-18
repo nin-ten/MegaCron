8c09587b7522e4b0881a40b47c2f67f2e67cd02f
Fukumasa Morifuji <ninten@Fukumasa-no-MacBook-Pro.local>
2014-02-18 10:28:06 +0900

    print schedules and heartbeats

--- status.py
+++ status.py
@@ -1,14 +1,52 @@
 #!/usr/bin/python
 
 import API
+import time
 
-def getWorkerStatus():
+def printWorkerStatus(w):
+    for worker in API.getWorkers():
+        if (w == worker):
+            debug_status("worker %d is up." % w.id)
+    debug_status("worker %d is down")
+
+def printAllUpWorkers():
     L = len(API.getWorkers())
     if (L == 1):
-        print "1 worker is up"
+        debug_status("1 worker is up")
     else:
-        print "%d workers are up" % L
+        debug_status("%d workers are up" % L)
+
+def printAllWorkerTask():
+    for w in API.getWorkers():
+        printWorkerTask(w)
+
+def printWorkerTask(w):
+        schedules = API.getSchedules(w)
+        string = "worker %d's schedules: " % w.id
+        for s in schedules:
+            string += "%d " % s.id,
+        debug_status(string)
+
+def printAllWorkerHeartBeat():
+    for w in API.getWorkers():
+        printWorkerHeartBeat(w)
+
+def printWorkerHeartBeat(w):
+        debug_status("worker %d's heartbeat: %s" % (w.id, w.heartbeat))
+
+def doall():
+    printAllUpWorkers()
+    printAllWorkerTask()
+    printAllWorkerHeartBeat()
+
+def do(w):
+    printWorkerStatus(w)
+    printWorkerTask(w)
+    printWorkerHeartBeat(w)
+
+def debug_status(s):
+    print "[status.py: %s] %s" % (time.ctime(), s)
+
 
 if __name__ == '__main__':
-    getWorkerStatus()
-
+    doall()
